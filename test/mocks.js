9d3144db8e553f40417ed685be6c6de122a4231c
Trevor Ford <info@trvrfrd.net>
2018-08-26 19:26:22 -0400

    improve mock data + update Cart snapshot

--- test/mocks.js
+++ test/mocks.js
@@ -1,23 +1,23 @@
 export const fakeItems = [
   {
     id: 1,
-    type: 'a',
+    type: 'product a',
     quantity: 1,
     price: 1.00,
-    description: 'product a'
+    description: 'amazing product'
   },
   {
     id: 2,
-    type: 'b',
+    type: 'product b',
     quantity: 2,
     price: 1.50,
-    description: 'product b'
+    description: 'beautiful product'
   },
   {
     id: 3,
-    type: 'c',
+    type: 'product c',
     quantity: 3,
     price: 2.00,
-    description: 'product c'
+    description: 'i love this product more than i love my wife'
   }
 ];
