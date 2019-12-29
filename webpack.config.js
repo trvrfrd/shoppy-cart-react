83a779a15928cfb10119b0821729a5b02e7efd81
Trevor Ford <info@trvrfrd.net>
2019-12-29 09:59:16 -0500

    call CleanWebpackPlugin v3 correctly in webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -37,7 +37,7 @@ module.exports = {
   },
 
   plugins: [
-    new CleanWebpackPlugin(['dist']),
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'src/index.html')
     })
